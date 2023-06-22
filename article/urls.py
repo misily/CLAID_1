2c4f4630a2651297be0ad909fa9e75411fb447ef
Kminy98 <pretty981210@gmail.com>
2023-06-22 10:20:28 +0900

    Fix: vocalarticle을 article로 변경
    - vocalarticle 삭제하고
    - article model을 수정
    
    related_to #58

--- article/urls.py
+++ article/urls.py
@@ -12,9 +12,6 @@ urlpatterns = [
     path('<int:article_id>/commentud/<int:pk>/', views.CommentViewByArticle.as_view(), name='CommentforTest'),
     path('<int:article_id>/commentcr/<int:comment_id>/good/', views.CommentGoodView.as_view(), name='comment_good_view'),
 
-    path('vocal/', views.VocalArticleView.as_view(), name='vocal_article_view'),
-    path('vocal/<int:article_id>/', views.VocalArticleDetailView.as_view(), name='vocal_article_detail_view'),
-
     path('notice/', views.VocalNoticeView.as_view(), name='vocal_notice_view'),
     path('notice/<int:article_id>/', views.VocalNoticeDetailView.as_view(), name='vocal_notice_detail_view'),
 ]
